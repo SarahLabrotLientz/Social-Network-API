c3cb70265b6f40bba0c97416a864a011b262b64d
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-03-15 18:33:25 -0400

    update code

--- utils/seed.js
+++ utils/seed.js
@@ -1,22 +1,22 @@
 const connection = require("../config/connection");
-const { Course, Student } = require("../models");
-const { getRandomName, getRandomAssignments } = require("./data");
+const { Thought, User } = require("../models");
+const { getRandomName, getRandomAssignments } = require("./data"); //should i delete this?
 
 connection.on("error", (err) => err);
 
 connection.once("open", async () => {
   console.log("connected");
 
-  // Drop existing courses
+  // Drop existing thoughts
   await Course.deleteMany({});
 
-  // Drop existing students
+  // Drop existing users
   await Student.deleteMany({});\
   
-  // Add students to the collection and await the results
+  // Add users to the collection and await the results
   await Student.collection.insertMany(students);
 
-  // Add courses to the collection and await the results
+  // Add thoughts to the collection and await the results
   await Course.collection.insertOne();
 
   // Log out the seed data to indicate what should appear in the database
